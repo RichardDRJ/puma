3c8edd8acc60c075b2612938bb950cc407e20c16
Richard Jones <joneseh25@gmail.com>
2015-05-18 02:44:14 +0100

    Updated numalloc_local to use numa_alloc_local if we're only allocating one page.

--- src/internal/numa.c
+++ src/internal/numa.c
@@ -1,4 +1,5 @@
 #include "internal/numa.h"
+#include "internal/pumanode.h"
 #include <unistd.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -7,11 +8,14 @@
 void* numalloc_local(size_t psize)
 {
 	void* ret;
-
+#if PUMA_NODEPAGES > 1 || !defined(NNUMA)
 	int status = posix_memalign(&ret, psize, psize);
 
 #ifndef NNUMA
 	numa_setlocal_memory(ret, psize);
+#endif // NNUMA
+#else
+	ret = numa_alloc_local(psize);
 #endif
 
 	assert(status == 0 || (printf("status = %d\n", status), false)); (void)status;
